fabac3d46782400addd8af8453cdd35f21e464a3
Kauã Luiz <kaua.kfk14@gmail.com>
2026-08-27 15:05:13 -0300

    implementado emailresult

---
+++ Configuration/EmailResult.cs
@@ -0,0 +1,8 @@
+namespace emailsender.Configuration
+{
+    public class EmailResult
+    {
+        public bool Resultado { get; set; }
+        public string Mensagem { get; set; } = string.Empty;
+    }
+}
